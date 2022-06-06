e77b35f40c9cfb9149f804fce76ef9f8b04a2b46
MDevTest <suehtam@28>
2022-06-06 19:56:23 -0300

    05

--- P2.c
+++ P2.c
@@ -3,62 +3,109 @@
 //Analise E Desenvolvimento De Sistema
 //UTFPR - Cornélio Procópio
 
-#include <stdio.h>
+#include <Stdio.h>
 #include <stdlib.h>
 
-#define mes = 30;
-
-typedef struct{
+struct eq{
     int codigoequipamento;
     char equipamento[30];
     float potencia;
-    int tempoativo;
     float consumonomes;
-}equipamento;
+    int tempoativo;
+}
 
-equipamento mallocvetor(int);
-equipamento reallocvetor(equipamento*, int);
+typedef struct eq equipamento;
+
+equipamento * alocavet(int);
+equipamento * realocavet(equipamento *, int);
+void preenche(equipamento[]);
+void relatorio(equipamento[]);
+
+int cont;
+int total;
 
 int main(){
-    int *v,tv = 1, i = 0, t = 5;
     char q;
-    equipamento *e;
-    e = mallocvetor(t);
-    do{
+    int i;
+    equipamento * V;
+
+    system("cls");
+    cont = 5;
+    V = alocavet(cont);
+    preenche(V);
+    printf("\nDeseja ver o relatorio? S/n");
+    scanf("%s",&q);
+    if(q == "S" || q == "s"){
         system("cls");
-        v[i].codigo = rand(); % 100;
-        printf("\nEquipamento:");
+        relatorio(V);
+        system("pause");
+        exit(0);
+    }else if(q == "N" || q == "n"){
+        system("cls");
+        printf("\nFim do programa");
+        system("pause");
+        exit(0);
+    }else{
+        system("cls");
+        printf("\nErro de parametros");
+        system("pause");
+        exit(0);
+    }
+    relatorio(V);
+
+    free(V);
+    return 0;
+}
+void preenche(equipamento vet[]){
+    int i = 0;
+    char q;
+    do{
+        vet[i].codigoequipamento = i + rand() + 1;
+        printf("\nNome do Equipamento:");
         fflush(stdin);
-        scanf("\t%s", &v[i].equipamento);
+        gets(vet[i].equipamento);
         do{
-            printf("\nPotencia em watts:");
-            scanf("\t%f", &v[i].potencia);
-        }while((v[i].potencia < 1) || (v[i].potencia >= 10000));
+            printf("\nPotencia do equipamento:");
+            scanf("%f",&vet[i].potencia);
+        }while(vet[i].potencia >= 1 || vet[i].potencia <= 10000);
         do{
-            printf("\nTempo de uso:");
-            scanf("\t%d", &v[i].tempoativo);
-        }while((v[i].tempoativo < 1) || (v[i].tempoativo >= 10000));
-        v[i].consumonomes = (v[i].potencia * (v[i].tempoativo / 60.0 * 30.0) / 1000.0);
-
-
-        printf("Deseja Adicionar mais algum equipamento?");
-        scanf("\n%s",&q);   
-        free(v);
-        tv = tv + 1;
-        i = i + 1;
-        if(i == t - 1){
-            t = t + 5;
-            v = reallocvetor(v,t);
-        }
-    }while(q == 's' || q == 'S');
-    exit(0);
+            printf("\nTempo de atividade registrada do equipamento:");
+            scanf("%d",&vet[i].tempoativo);
+        }while(vet[i].tempoativo >= 1 || vet[i].tempoativo <= 10000);
+        vet[i].consumonomes = vet[i].potencia * (vet[i].tempoativo / 60.0 * 30.0 / 1000.0);
+        printf("\n\nDeseja realizar novo cadastro? S/n");
+        scanf("%s",&q);
+        system("cls");
+        total = i;
+    }while(q == "S" || q == "s");
 }
-equipamento* mallocvetor(int tv){
-    equipamento* v
-    v = malloc(tv * sizeof(equipamento));
-    return v;
+void relatorio(equipamento vet[]){
+    int i = 0;
+    printf("\nRelatorio de equipamentos");
+    for(i = 0; i < total; i++){
+        printf("\n=========================");
+        printf("\nCodigo......: %d",vet[i].codigoequipamento);
+        printf("\nEquipamento.: %s",vet[i].equipamento);
+        printf("\nPotencia....: %2.2f",vet[i].potencia);
+        printf("\nTempo ativo.: %d",vet[i].tempoativo);
+        printf("\nConsumo.....: %2.2f",vet[i].consumonomes);
+    }
 }
-equipamento* reallocvetor(equipamento *v, int tv){
-    v = realloc(v, tv * sizeof(equipamento));
-    return v;
+equipamento * alocavet(int tam){
+    equipamento *vet;
+    if(malloc(tam * sizeof(equipamento)) == NULL){
+        printf("\nNao ha memoria disponivel");
+        exit(0);
+    }
+    vet = malloc(tam * sizeof(equipamento));
+    return vet;
 }
+equipamento * realocavet(equipamento *vet, int tam){
+    if(realloc(vet,tam * sizeof(equipamento)) == NULL){
+        printf("\nNao ha memoria disponivel");
+        exit(0);
+    }
+    vet = realloc(vet,tam * sizeof(equipamento));
+    return vet;
+}
+preenche
